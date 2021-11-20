98b97aa56fcaab6ae3576dc4ef3049fc7b277efb
Setio345 <94732911+Setio345@users.noreply.github.com>
2021-11-20 13:51:01 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = 'your_telegram_token';
+const token = 2118568007:AAHAcOf3YIGlDeNrBAf7KPRkJDDu2KIa_tU;
 const bot = new TelegramBot(token, {polling: true});
 
 
