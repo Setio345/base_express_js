739c42dd73b5a999cefd7420a7e82e4c040760fb
Setio345 <94732911+Setio345@users.noreply.github.com>
2021-11-24 14:51:27 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -25,8 +25,8 @@ bot.onText(/\/JII/,(msg) => {
     bot.sendMessage(
         global_msg_id,
         `
-            ACES : ACE HARDWARE INDONESIA \n
-            ADRO : ADARO ENERGY //https://www.adaro.com/\n
+            ACES : ACE HARDWARE INDONESIA https://www.acehardware.co.id/\n
+            ADRO : ADARO ENERGY https://www.adaro.com/\n
             AKRA : AKR CORPORINDO \n
             ANTM : ANEKA TAMBANG \n
             BRIS : BANK SYARIAH INDONESIA \n
