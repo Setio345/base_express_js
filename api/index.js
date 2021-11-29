63c7b4af9b8cd3aff1ac4fe288f9ac773031d003
Setio345 <94732911+Setio345@users.noreply.github.com>
2021-11-29 10:21:06 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,8 +14,42 @@ bot.onText(/\/start/, (msg) => {
     global_msg_id = msg.chat.id;
     bot.sendMessage(
         global_msg_id,
-        `hello ${msg.chat.first_name}, welcome...\n
-        click /predict`
+`hello ${msg.chat.first_name},\n
+selamat datang di index pasar saham indonesia \n
+click /JII \n
+click /LQ45`
+    );
+});
+
+bot.onText(/\/JII/,(msg) => {
+    global_msg_id = msg.chat.id;
+    bot.sendMessage(
+        global_msg_id,
+        `
+click /ACES : ACE HARDWARE INDONESIA \n
+click /ADRO : ADARO ENERGY \n
+click /AKRA : AKR CORPORINDO \n
+click /ANTM : ANEKA TAMBANG \n
+click /ERAA : ERAJAYA SWASEMBADA \n
+click /EXCL : XL AXIATA \n
+click /ICBP : INDOFOOD CBP SUKSES MAKMUR
+        `
+    );
+});
+
+bot.onText(/\/LQ45/, (msg) => {
+    global_msg_id = msg.chat.id;
+    bot.sendMessage(
+        global_msg_id,
+        `
+click /ACES : ACE HARDWARE INDONESIA \n
+click /ADRO : ADARO ENERGY \n
+click /AKRA : AKR CORPORINDO \n
+click /ANTM : ANEKA TAMBANG \n
+click /ASII : ASTRA INTERNATIONAL \n
+click /EXCL : XL AXIATA \n
+click /INDF : INDOFOOD SUKSES MAKMUR
+        `
     );
 });
 
@@ -32,35 +66,121 @@ router.get('/', (req, res, next) => {
   });
 });
 
-
-router.get('/sensor/:sensor1/:sensor2/:sensor3', (req, res, next) => {
-  try {
-      bot.sendMessage(
-            global_msg_id, //msg.id
-            `Pembacaan Sensor:: ${req.params.sensor1}, ${req.params.sensor2}, ${req.params.sensor3}`
-     );
-      res.json({
-        "status": 202,
-        "messgae": "Success",
-        "data": {
-          "sensor_1": req.params.sensor1,
-          "sensor_2": req.params.sensor2,
-          "sensor_3": req.params.sensor3
-        }
-      });
-  } catch (err) {
-      next(err);
-  }
-});
-
-
-router.get('/test/:key', function(req, res, next){
+bot.onText(/\/ACES/,(msg) => {
+    global_msg_id = msg.chat.id;
     bot.sendMessage(
-            global_msg_id, //msg.id
-            `${req.params.key}`
+        global_msg_id,
+        `
+summary    : https://finance.yahoo.com/quote/aces.jk/\n
+chart      : https://finance.yahoo.com/quote/aces.JK/chart?p=ACES.JK/\n
+statistic  : https://finance.yahoo.com/quote/aces.JK/key-statistics?p=ACES.JK/\n
+Historical : https://finance.yahoo.com/quote/aces.JK/history?p=ACES.JK/
+        `
     );
-    res.json(req.params.key);
 });
 
+bot.onText(/\/ADRO/,(msg) => {
+    global_msg_id = msg.chat.id;
+    bot.sendMessage(
+        global_msg_id,
+        `
+summary    : https://finance.yahoo.com/quote/adro.jk/ \n
+chart      : https://finance.yahoo.com/quote/ADRO.JK/chart?p=ADRO.JK/\n
+statistic  : https://finance.yahoo.com/quote/ADRO.JK/key-statistics?p=ADRO.JK/\n
+Historical : https://finance.yahoo.com/quote/ADRO.JK/history?p=ADRO.JK/
+        `
+    );
+});
 
+bot.onText(/\/AKRA/,(msg) => {
+    global_msg_id = msg.chat.id;
+    bot.sendMessage(
+        global_msg_id,
+        `
+summary    : https://finance.yahoo.com/quote/akra.jk/\n
+chart      : https://finance.yahoo.com/quote/akra.JK/chart?p=AKRA.JK/\n
+statistic  : https://finance.yahoo.com/quote/akra.JK/key-statistics?p=AKRA.JK/\n
+Historical : https://finance.yahoo.com/quote/akra.JK/history?p=AKRA.JK/
+        `
+    );
+});
+
+bot.onText(/\/ANTM/,(msg) => {
+    global_msg_id = msg.chat.id;
+    bot.sendMessage(
+        global_msg_id,
+        `
+summary    : https://finance.yahoo.com/quote/antm.jk/\n
+chart      : https://finance.yahoo.com/quote/antm.JK/chart?p=ANTM.JK/\n
+statistic  : https://finance.yahoo.com/quote/antm.JK/key-statistics?p=ANTM.JK/\n
+Historical : https://finance.yahoo.com/quote/antm.JK/history?p=ANTM.JK/
+        `
+    );
+});
+
+bot.onText(/\/EXCL/,(msg) => {
+    global_msg_id = msg.chat.id;
+    bot.sendMessage(
+        global_msg_id,
+        `
+summary    : https://finance.yahoo.com/quote/excl.jk/\n
+chart      : https://finance.yahoo.com/quote/excl.JK/chart?p=EXCL.JK/\n
+statistic  : https://finance.yahoo.com/quote/excl.JK/key-statistics?p=EXCL.JK/\n
+Historical : https://finance.yahoo.com/quote/excl.JK/history?p=EXCL.JK/
+        `
+    );
+});
+
+bot.onText(/\/ERAA/,(msg) => {
+    global_msg_id = msg.chat.id;
+    bot.sendMessage(
+        global_msg_id,
+        `
+summary    : https://finance.yahoo.com/quote/eraa.jk/\n
+chart      : https://finance.yahoo.com/quote/eraa.JK/chart?p=ERAA.JK/\n
+statistic  : https://finance.yahoo.com/quote/eraa.JK/key-statistics?p=ERAA.JK/\n
+Historical : https://finance.yahoo.com/quote/eraa.JK/history?p=ERAA.JK/
+
+        `
+    );
+});
+
+bot.onText(/\/ICBP/,(msg) => {
+    global_msg_id = msg.chat.id;
+    bot.sendMessage(
+        global_msg_id,
+        `
+summary    : https://finance.yahoo.com/quote/icbp.jk/\n
+chart      : https://finance.yahoo.com/quote/icbp.JK/chart?p=ICBP.JK/\n
+statistic  : https://finance.yahoo.com/quote/icbp.JK/key-statistics?p=ICBP.JK/\n
+Historical : https://finance.yahoo.com/quote/icbp.JK/history?p=ICBP.JK/
+        `
+    );
+});
+
+bot.onText(/\/ASII/,(msg) => {
+    global_msg_id = msg.chat.id;
+    bot.sendMessage(
+        global_msg_id,
+        `
+summary    : https://finance.yahoo.com/quote/asii.jk/\n
+chart      : https://finance.yahoo.com/quote/asii.JK/chart?p=ASII.JK/\n
+statistic  : https://finance.yahoo.com/quote/asii.JK/key-statistics?p=ASII.JK/\n
+Historical : https://finance.yahoo.com/quote/asii.JK/history?p=ASII.JK/
+        `
+    );
+});
+
+bot.onText(/\/INDF/,(msg) => {
+    global_msg_id = msg.chat.id;
+    bot.sendMessage(
+        global_msg_id,
+        `
+summary    : https://finance.yahoo.com/quote/indf.jk/\n
+chart      : https://finance.yahoo.com/quote/indf.JK/chart?p=INDF.JK/\n
+statistic  : https://finance.yahoo.com/quote/indf.JK/key-statistics?p=INDF.JK/\n
+Historical : https://finance.yahoo.com/quote/indf.JK/history?p=INDF.JK/
+        `
+    );
+});
 module.exports = router;
